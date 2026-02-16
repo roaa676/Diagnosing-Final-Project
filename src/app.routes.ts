754c589cc016ff9d0f82571739a1ef8e25be38a5
Roaa71 <roaaezzat676@gmail.com>
2026-02-17 00:37:24 +0200

    create ts , html files for dashboard

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,23 +1,24 @@
 import { Routes } from '@angular/router';
-import { AppLayout } from './app/layout/component/app.layout';
-import { Dashboard } from './app/pages/dashboard/dashboard';
+import { AppLayout } from './app/core/shared/layout/app-layout';
 import { Documentation } from './app/pages/documentation/documentation';
-import { Landing } from './app/pages/landing/landing';
 import { Notfound } from './app/pages/notfound/notfound';
+import { Dashboard } from '@/pages/Dashboard/dashboard';
+import { Dashboardd } from '@/pages/dashboardd/dashboardd';
+
 
 export const appRoutes: Routes = [
     {
         path: '',
         component: AppLayout,
         children: [
-            { path: '', component: Dashboard },
+            { path: '', component: Dashboardd },
             { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
             { path: 'documentation', component: Documentation },
             { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
         ]
     },
-    { path: 'landing', component: Landing },
+    { path: 'dashboard', component: Dashboard },
     { path: 'notfound', component: Notfound },
     { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
-    { path: '**', redirectTo: '/notfound' }
+    { path: '**', redirectTo: '/dashboard' }
 ];
