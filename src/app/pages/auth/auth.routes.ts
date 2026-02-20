092a4c816cf9cf7b57b97d4dc4e6ad9a626b82d2
douhaaladdin <douhaaladdin@gmail.com>
2026-02-20 02:50:09 +0200

    auth.routes.ts

--- src/app/pages/auth/auth.routes.ts
+++ src/app/pages/auth/auth.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { Login } from './Login/login';
-import { RegisterComponent } from './Register/register';
+import { Register } from './Register/register';
 
-// auth.routes.ts
-export const AuthRoutes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-];
+export default [
+    { path: 'login', component: Login },
+    { path: 'register', component: Register }
+] as Routes;
