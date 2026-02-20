b51e015b94242396e8786be24e0ccff6e0ad6f0f
Roaa71 <roaaezzat676@gmail.com>
2026-02-20 03:32:11 +0200

    fix issue in register html file

--- src/app/core/shared/layout/Topbar/app-topbar.ts
+++ src/app/core/shared/layout/Topbar/app-topbar.ts
@@ -3,14 +3,12 @@ import { MenuItem } from 'primeng/api';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { StyleClassModule } from 'primeng/styleclass';
-import { AppConfigurator } from '../../../../layout/component/app.configurator';
 import { LayoutService } from '../../../../layout/service/layout.service';
-import { Button } from "primeng/button";
 
 @Component({
     selector: 'app-topbar',
     standalone: true,
-    imports: [RouterModule, CommonModule, StyleClassModule, Button],
+    imports: [RouterModule, CommonModule, StyleClassModule],
     templateUrl: './app-topbar.html',
 })
 
--- src/app/pages/auth/auth.routes.ts
+++ src/app/pages/auth/auth.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { Login } from './Login/login';
-import { Register } from './Register/register';
+import { RegisterComponent } from './Register/register';
 
 export default [
     { path: 'login', component: Login },
-    { path: 'register', component: Register }
+    { path: 'register', component: RegisterComponent }
 ] as Routes;
