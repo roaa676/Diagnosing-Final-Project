1543ec9ce38d1e90aaf1e81e42d8f8dc2b2f6614
douhaaladdin <douhaaladdin@gmail.com>
2026-03-15 00:10:30 +0200

    training.html

--- src/app/pages/auth/auth.routes.ts
+++ src/app/pages/auth/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './Login/login';
 import { RegisterComponent } from './Register/register';
+import { TrainingComponent } from './training/training';
 
 export default [
     { path: 'login', component: Login },
-    { path: 'register', component: RegisterComponent }
+    { path: 'register', component: RegisterComponent },
+    { path: 'training', component: TrainingComponent }
 ] as Routes;
---
+++ src/app/pages/auth/training/training.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-training',
+  standalone: true,
+  imports: [],
+  templateUrl: './training.html',
+ styleUrls: ['./training.css']
+})
+export class TrainingComponent {
+
+  constructor() { }
+
+}
